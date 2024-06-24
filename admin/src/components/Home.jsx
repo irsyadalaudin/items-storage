120c4d967611b79069eac5a6d09b46f5a652da96
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-06-24 12:33:02 +0300

    added homeBg as home background image

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -1,9 +1,12 @@
+import homeBg from '../assets/homeBg.jpg'
+
 const Home = () => {
     return (
-        // <div className='home-container mt-10 mb-10 px-28'>
-        <div className='mt-10 mb-10 px-28 bg-[url("./assets/home.jpg")]'>
-            <h1>Selamat datang di HAR company</h1>
-            <h3>Disini admin dapat melakukan peng-inputan barang masuk, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer</h3>
+        <div className='mt-10 px-28 relative'>
+            <img className='' src={homeBg} alt='' />
+            {/* <img className='w-97 h-97,5' src={homeBg} alt='' /> */}
+            {/* <h1>Selamat datang di HAR company</h1>
+            <h3>Disini admin dapat melakukan peng-inputan barang masuk, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer</h3> */}
         </div>
     )
 }
