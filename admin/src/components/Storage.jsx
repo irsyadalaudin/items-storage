1b7459611b6e1a9e3958ce5396e1703aee34353c
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-07-02 13:45:32 +0300

    renmoved wrong shadow

--- admin/src/components/Storage.jsx
+++ admin/src/components/Storage.jsx
@@ -96,7 +96,7 @@ const Storage = () => {
 
     return (
         <div className='flex justify-center px-28 text-lg'>
-            <form onSubmit={handleSubmit} className='flex justify-around bg-[#F3F4F6] p-5 mb-10 rounded-b-md rounded-tr-md shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] absolute top-560 w-1000 z-8 '>
+            <form onSubmit={handleSubmit} className='flex justify-around bg-[#F3F4F6] p-5 mb-10 rounded-b-md rounded-tr-md absolute top-560 w-1000 z-8 '>
 			<button className='absolute h-10 bottom-960 left-0 z-9 bg-[#F3F4F6] text-black rounded-t-md border-none' disabled>Data Barang Masuk</button>
                 {/* <h2 className='m-0 p-0'>Data barang masuk</h2> */}
 				<div>
