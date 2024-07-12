0b460b3fd840bc5dd1d8364f82987207806d117a
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-07-12 15:42:17 +0300

    changed bg color into bg-amber-800

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className='mt-10 mb-16 px-28 relative'>
             <img className='' src={homeBg} alt='' />
-            <div className='bg-amber-700 rounded w-430 h-176'>
+            <div className='bg-amber-800 rounded w-430 h-176'>
                 <h2>Selamat datang di HAR company</h2>
                 <h4>Disini admin dapat melakukan peng-inputan barang masuk, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer</h4>
             </div>
