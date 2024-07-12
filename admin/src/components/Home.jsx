39b9b34b6e060b731fa94e9f9e2f8a6281b0332d
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-07-12 15:39:42 +0300

    added h2 and h4 text

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -4,9 +4,10 @@ const Home = () => {
     return (
         <div className='mt-10 mb-16 px-28 relative'>
             <img className='' src={homeBg} alt='' />
-            {/* <img className='w-97 h-97,5' src={homeBg} alt='' /> */}
-            {/* <h1>Selamat datang di HAR company</h1>
-            <h3>Disini admin dapat melakukan peng-inputan barang masuk, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer</h3> */}
+            <div className='bg-amber-700 rounded w-430 h-176'>
+                <h2>Selamat datang di HAR company</h2>
+                <h4>Disini admin dapat melakukan peng-inputan barang masuk, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer</h4>
+            </div>
         </div>
     )
 }
