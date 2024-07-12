6bcca31c4602b325be6a6aa25e04237c7e58c804
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-07-12 15:23:10 +0300

    added mb-16

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -2,7 +2,7 @@ import homeBg from '../assets/home-bg.jpg'
 
 const Home = () => {
     return (
-        <div className='mt-10 px-28 relative'>
+        <div className='mt-10 mb-16 px-28 relative'>
             <img className='' src={homeBg} alt='' />
             {/* <img className='w-97 h-97,5' src={homeBg} alt='' /> */}
             {/* <h1>Selamat datang di HAR company</h1>
